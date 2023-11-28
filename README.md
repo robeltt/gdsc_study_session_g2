a5b76f983888efb172de875933c883fd53716804
robeltt <robel.teshome83@gmail.com>
2023-11-28 20:47:40 +0300

    BMI calculator

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # gdsc_study_session_g2
-
-The first task of GDSC study session
---
+++ script.js
@@ -0,0 +1,6 @@
+
+const mass = prompt("what is your mass");
+const height = +prompt("what is your height");
+const BMI = mass / height ** 2;
+console.log(BMI);
+console.log(typeof height);
